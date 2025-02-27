4400c3546414b94e598fc0f1f04614d8290095dc
Korede Ogundele <korede_ogundele@berkeley.edu>
2025-02-27 17:13:30 -0500

    edited gitignore. let's ignore txt files for now

--- ML-for-Life-Sciences/01_molecule_toxicity.py
+++ ML-for-Life-Sciences/01_molecule_toxicity.py
@@ -1,24 +1,23 @@
-# I can't get the deepchem package to work due to dependency issues. I am working it out within the jupyter notebook.
-# In the meantime, I guess it's valuable to type out the full code here? Idk, just learning as I go.
+# This short script builds and evaluates a model that predicts molecule toxicity.
 
 import numpy as np
 import deepchem as dc
 
-# load data and split into datasets
-# following line is original code from textbook. Gave the error: DEPRECATION WARNING: please use MorganGenerator
-# after some trial and error, I found circular (morgan) fingerprints
-#tox21_tasks, tox21_datasets, transformers = dc.molnet.load_tox21()
-training_dataset, validation_dataset, test_dataset = tox21_datasets
+# load data from Tox21 dataset. Split into tasks, datasets & transformers
+tox21_tasks, tox21_datasets, transformers = dc.molnet.load_tox21()
+training_dataset, validsation_dataset, test_dataset = tox21_datasets
 
-# build and train model
+
+# DeepChem's dc.models contains many different life science-specific models. Here we use MultitaskClassifier
 model = dc.models.MultitaskClassifier(n_tasks=12, n_features=1024, layer_sizes=[1000]) # using textbook numbers right now, might mess with these a bit
 model.fit(training_dataset, nb_epoch=10)
 
-# evaluate model
+# evaluate performance using ROC (receiver opeating characteristic) AUC across all tasks
 metric = dc.metrics.Metric(dc.metrics.roc_auc_score, np.mean)
 
+# get scores using model.evaluate
 training_scores = model.evaluate(training_dataset, [metric], transformers)
 test_scores = model.evaluate(test_dataset, [metric], transformers)
 
-print(f"Training dataset scores are: {training_scores}")
-print(f"Test dataset scores are: {test_scores}")
+print(f"Training {training_scores}")
+print(f"Test {test_scores}")
