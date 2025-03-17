39bb6b1436aca131127a3e99e9aa7d491912ba36
Korede Ogundele <korede_ogundele@berkeley.edu>
2025-03-17 11:13:41 -0400

    replaced PDFS with jupyter notebooks

---
+++ ML-for-Life-Sciences/02_mnist.py
@@ -0,0 +1,5 @@
+# Using the classic MNIST dataset. I'm gonna build a CNN from scratch (throwback to chem277b)
+
+import deepchem as dc
+import tensorflow as tf
+import tensorflow.keras.layers as layers
