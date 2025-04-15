4c277159b4d45a97133dc997757842170ca8a3ed
Korede Ogundele <korede_ogundele@berkeley.edu>
2025-04-15 17:30:57 -0400

    finished pysmiles reading portion

--- pysmiles.py
+++ pysmiles.py
@@ -4,5 +4,15 @@
 
 from pysmiles import read_smiles
 
+# READING SMILES
 smiles = 'C1CC[13CH2]CC1C1CCCCC1'
 mol = read_smiles(smiles)
+
+# molecule is a networkx graph, so nodes are elements, bonds are edges
+print(f"Elements (excluding hydrogen) in the molecule are: {mol.nodes(data='element')}")
+print(f"Bonds in molecule are: {mol.nodes(data='hcount')}")
+
+mol_with_H = read_smiles(smiles, explicit_hydrogen=True)
+print(f"Elements (including hydrogen) in the molecule are: {mol_with_H.nodes(data='element')}")
+
+# WRITING SMILES
